fb44a4c3612e7c777c4bf191f3e040a96ded4a2b
Caio Pra <caioprsilva@gmail.com>
2022-06-14 18:43:13 -0300

    Leassons 23 and 24 of C# course

--- curso-C_sharp/secao3/Aula22_23/Aula22.cs
+++ curso-C_sharp/secao3/Aula22_23/Aula22.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+
 namespace Aula22_23
 {
     class Aula22
@@ -11,6 +13,18 @@ namespace Aula22_23
             string s = Console.ReadLine();
             string[] vet = s.Split(' ');
             System.Console.WriteLine(vet[0] + vet[1] + vet[2]);
+
+            // parte da aula 23
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine()); 
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+            console.WriteLine($"{nome} {sexo} {idade} {altura}");
         }
     }
 }
---
+++ curso-C_sharp/secao3/AulasSecao3/AulasSecao3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace AulasSecao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //string frase = Console.ReadLine();
+            //System.Console.WriteLine(frase);
+
+            //string s = Console.ReadLine();
+            //string[] vet = s.Split(' ');
+            //System.Console.WriteLine(vet[0] + vet[1] + vet[2]);
+
+            //parte da aula 23
+            //int n1 = int.Parse(Console.ReadLine());
+            //char ch = char.Parse(Console.ReadLine());
+            //double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Console.WriteLine(n1 + ch + n2);
+
+            //string[] vet = Console.ReadLine().Split(' ');
+            //string nome = vet[0];
+            //char sexo = char.Parse(vet[1]);
+            //int idade = int.Parse(vet[2]);
+            //double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+            //Console.WriteLine($"{nome} {sexo} {idade} {altura}");
+
+            // aula 23
+            //Console.WriteLine("Entre com seu nome completo: ");
+            //string nome = Console.ReadLine();
+            //Console.WriteLine($"Olá, {nome}");
+
+            //Console.WriteLine("Quantos quartos tem na sua casa?");
+            //int idade = int.Parse(Console.ReadLine());
+            //Console.WriteLine(idade);
+
+            //Console.WriteLine("Entre com o preço de um produto:");
+            //double preco = double.Parse(Console.ReadLine());
+            //// com InvariantCulture usa , como separacao em numeros decimais
+            //Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            //Console.WriteLine("Entre seu úlitmo nome, idade e altura:");
+            //string[] vet = Console.ReadLine().Split(' ');
+
+            //string sobrenome = vet[0];
+            //int idade2 = int.Parse(vet[1]);
+            //double altura = double.Parse(vet[2]);
+
+            //Console.WriteLine(sobrenome);
+            //Console.WriteLine(idade2);
+            //Console.WriteLine(altura);
+
+
+        }
+    }
+}
