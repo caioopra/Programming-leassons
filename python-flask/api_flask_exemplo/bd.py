d098e6de675df99462f1773aaa2fc915958d5868
Caio <caioprsilva@gmail.com>
2023-08-14 08:14:51 -0300

    Continuing flask

---
+++ python-flask/api_flask_exemplo/bd.py
@@ -0,0 +1,7 @@
+Carros = [
+    {"id": 1, "marca": "Fiat", "modelo": "Marea", "ano": 1997},
+    {"id": 2, "marca": "Fiat", "modelo": "Uno", "ano": 1992},
+    {"id": 3, "marca": "Ford", "modelo": "Escort", "ano": 1985},
+    {"id": 4, "marca": "Chevrolet", "modelo": "Chevetter", "ano": 1978},
+    {"id": 5, "marca": "Volkaswagen", "modelo": "Fusca", "ano": 1962},
+]
