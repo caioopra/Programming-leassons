97c4395f0054f308d2e628bb4932776a2abf126a
caioopra <caioprsilva@gmail.com>
2023-08-16 15:57:39 -0300

    qt window

---
+++ python-pyqt5/codemy_com/window1.py
@@ -0,0 +1,53 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from window2 import Ui_SecondWindow
+from window3 import Ui_Dialog
+
+class Ui_MainWindow(object):
+ #   def openWindow(self):
+ #       self.window = QtWidgets.QMainWindow()
+ #       self.ui = Ui_SecondWindow()
+ #       self.ui.setupUi(self.window)
+ #       self.window.show()
+    def openWindow(self):
+        self.window = QtWidgets.QDialog()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self.window)
+        self.window.show()
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(601, 392)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget, clicked = lambda: self.openWindow())
+        self.pushButton.setGeometry(QtCore.QRect(180, 100, 211, 111))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 601, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Open Window"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
